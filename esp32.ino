8a637c62e045c9e382a45207731ec1f647b4c308
faqris <129918087+faqris@users.noreply.github.com>
2023-06-26 22:10:03 +0800

    Update esp32.ino

--- esp32.ino
+++ esp32.ino
@@ -1,65 +1,126 @@
 #include <WiFi.h>
+#include <WiFiClientSecure.h>
 #include <PubSubClient.h>
 #include <NewPing.h>
 
 // Ultrasonic sensor configuration
-#define TRIGGER_PIN D5
-#define ECHO_PIN D6
+#define TRIGGER_PIN 2
+#define ECHO_PIN 15
 #define MAX_DISTANCE 200
 
-// Wi-Fi credentials
-const char* ssid = "WIFI_SSID";
-const char* password = "WIFI_PASSWORD";
-
-// MQTT broker configuration
-const char* mqtt_broker = "a5da1b92468e4cd6b6dd31c7909a5cc6.s2.eu.hivemq.cloud";
-const int mqtt_port = 8883;
-mqtt_topic = "floodsentry";
-const char* mqtt_username = "admin";
-const char* mqtt_password = "Admin1234";
-
-WiFiClient espClient;
-PubSubClient mqttClient(espClient);
-
 NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
+ 
+static const char *root_ca PROGMEM = R"EOF(
+-----BEGIN CERTIFICATE-----
+MIIFazCCA1OgAwIBAgIRAIIQz7DSQONZRGPgu2OCiwAwDQYJKoZIhvcNAQELBQAw
+TzELMAkGA1UEBhMCVVMxKTAnBgNVBAoTIEludGVybmV0IFNlY3VyaXR5IFJlc2Vh
+cmNoIEdyb3VwMRUwEwYDVQQDEwxJU1JHIFJvb3QgWDEwHhcNMTUwNjA0MTEwNDM4
+WhcNMzUwNjA0MTEwNDM4WjBPMQswCQYDVQQGEwJVUzEpMCcGA1UEChMgSW50ZXJu
+ZXQgU2VjdXJpdHkgUmVzZWFyY2ggR3JvdXAxFTATBgNVBAMTDElTUkcgUm9vdCBY
+MTCCAiIwDQYJKoZIhvcNAQEBBQADggIPADCCAgoCggIBAK3oJHP0FDfzm54rVygc
+h77ct984kIxuPOZXoHj3dcKi/vVqbvYATyjb3miGbESTtrFj/RQSa78f0uoxmyF+
+0TM8ukj13Xnfs7j/EvEhmkvBioZxaUpmZmyPfjxwv60pIgbz5MDmgK7iS4+3mX6U
+A5/TR5d8mUgjU+g4rk8Kb4Mu0UlXjIB0ttov0DiNewNwIRt18jA8+o+u3dpjq+sW
+T8KOEUt+zwvo/7V3LvSye0rgTBIlDHCNAymg4VMk7BPZ7hm/ELNKjD+Jo2FR3qyH
+B5T0Y3HsLuJvW5iB4YlcNHlsdu87kGJ55tukmi8mxdAQ4Q7e2RCOFvu396j3x+UC
+B5iPNgiV5+I3lg02dZ77DnKxHZu8A/lJBdiB3QW0KtZB6awBdpUKD9jf1b0SHzUv
+KBds0pjBqAlkd25HN7rOrFleaJ1/ctaJxQZBKT5ZPt0m9STJEadao0xAH0ahmbWn
+OlFuhjuefXKnEgV4We0+UXgVCwOPjdAvBbI+e0ocS3MFEvzG6uBQE3xDk3SzynTn
+jh8BCNAw1FtxNrQHusEwMFxIt4I7mKZ9YIqioymCzLq9gwQbooMDQaHWBfEbwrbw
+qHyGO0aoSCqI3Haadr8faqU9GY/rOPNk3sgrDQoo//fb4hVC1CLQJ13hef4Y53CI
+rU7m2Ys6xt0nUW7/vGT1M0NPAgMBAAGjQjBAMA4GA1UdDwEB/wQEAwIBBjAPBgNV
+HRMBAf8EBTADAQH/MB0GA1UdDgQWBBR5tFnme7bl5AFzgAiIyBpY9umbbjANBgkq
+hkiG9w0BAQsFAAOCAgEAVR9YqbyyqFDQDLHYGmkgJykIrGF1XIpu+ILlaS/V9lZL
+ubhzEFnTIZd+50xx+7LSYK05qAvqFyFWhfFQDlnrzuBZ6brJFe+GnY+EgPbk6ZGQ
+3BebYhtF8GaV0nxvwuo77x/Py9auJ/GpsMiu/X1+mvoiBOv/2X/qkSsisRcOj/KK
+NFtY2PwByVS5uCbMiogziUwthDyC3+6WVwW6LLv3xLfHTjuCvjHIInNzktHCgKQ5
+ORAzI4JMPJ+GslWYHb4phowim57iaztXOoJwTdwJx4nLCgdNbOhdjsnvzqvHu7Ur
+TkXWStAmzOVyyghqpZXjFaH3pO3JLF+l+/+sKAIuvtd7u+Nxe5AW0wdeRlN8NwdC
+jNPElpzVmbUq4JUagEiuTDkHzsxHpFKVK7q4+63SM1N95R1NbdWhscdCb+ZAJzVc
+oyi3B43njTOQ5yOf+1CceWxG1bQVs5ZufpsMljq4Ui0/1lvh+wjChP4kqKOJ2qxq
+4RgqsahDYVvTH9w7jXbyLeiNdd8XM2w9U/t7y0Ff/9yi0GE44Za4rF2LN9d11TPA
+mRGunUHBcnWEvgJBQl9nJEiU0Zsnvgc/ubhPgXRR4Xq37Z0j4r7g1SgEEzwxA57d
+emyPxgcYxn/eR44/KJ4EBs+lVDR3veyJm+kXQ99b21/+jh5Xos1AnX5iItreGCc=
+-----END CERTIFICATE-----
+)EOF";
 
-void setup() {
-  Serial.begin(115200);
-
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.println("Connecting to WiFi...");
-  }
-
-  mqttClient.setServer(mqtt_broker, mqtt_port);
-  while (!mqttClient.connected()) {
-    if (mqttClient.connect("ESP32Client", mqtt_username, mqtt_password)) {
-      Serial.println("Connected to MQTT broker");
-    } else {
-      Serial.print("Failed, rc=");
-      Serial.print(mqttClient.state());
-      Serial.println("Retrying in 5 seconds...");
-      delay(5000);
-    }
-  }
-}
-
-void loop() {
-  delay(2000);
+// Replace with your network credentials 
+const char* ssid = "NodeMCU"; 
+const char* password = "12345678"; 
+ 
+// Replace with your Hive MQTT broker details 
+const char* mqttServer = "a5da1b92468e4cd6b6dd31c7909a5cc6.s2.eu.hivemq.cloud"; 
+const int mqttPort = 8883; 
+ 
+// Replace with your Hive MQTT credentials 
+const char* mqttUsername = "admin"; 
+const char* mqttPassword = "Admin1234"; 
+ 
+WiFiClientSecure espClient; 
+PubSubClient client(espClient); 
+ 
+void setup() { 
+  Serial.begin(115200); 
+  setupWifi(); 
+  espClient.setCACert(root_ca);
+  client.setServer(mqttServer, mqttPort); 
+  //client.setCallback(callback); 
+} 
+ 
+void loop() { 
+  delay(50);
 
   unsigned int distance = sonar.ping_cm();
   Serial.print("Distance: ");
   Serial.print(distance);
   Serial.println(" cm");
-
-  if (mqttClient.connected()) {
+if (client.connected()) {
     String payload = String(distance);
-    mqttClient.publish(mqtt_topic + "/ultrasonic", payload.c_str());
+    client.publish("floodsentry/ultrasonic", payload.c_str());
   } else {
     Serial.println("MQTT connection lost. Reconnecting...");
-    if (mqttClient.connect("ESP32Client", mqtt_username, mqtt_password)) {
-      Serial.println("Connected to MQTT broker");
-    }
+    reconnect();
   }
+  //client.loop(); 
+} 
+ 
+void setupWifi() { 
+  delay(10); 
+  Serial.println(); 
+  Serial.print("Connecting to "); 
+  Serial.println(ssid); 
+  WiFi.begin(ssid, password); 
+  while (WiFi.status() != WL_CONNECTED) { 
+    delay(500); 
+    Serial.print("."); 
+  } 
+  Serial.println(""); 
+  Serial.println("WiFi connected"); 
+  Serial.println("IP address: "); 
+  Serial.println(WiFi.localIP()); 
+} 
+ 
+void reconnect() { 
+  while (!client.connected()) { 
+    Serial.print("Attempting MQTT connection..."); 
+    if (client.connect("ESP32Client", mqttUsername, mqttPassword)) { 
+      Serial.println("connected"); 
+      client.subscribe("floodsentry/ultrasonic"); 
+    } else { 
+      Serial.print("failed, rc="); 
+      Serial.print(client.state()); 
+      Serial.println(" try again in 5 seconds"); 
+      delay(5000); 
+    } 
+  } 
+} 
+ 
+void callback(char* topic, byte* payload, unsigned int length) { 
+  Serial.print("Message received on topic: "); 
+  Serial.println(topic); 
+  Serial.print("Message: "); 
+  for (int i = 0; i < length; i++) { 
+    Serial.print((char)payload[i]); 
+  } 
+  Serial.println(); 
 }
